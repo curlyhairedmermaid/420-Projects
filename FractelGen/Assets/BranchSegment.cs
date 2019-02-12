96fc69f6b13d0d5fa8e1d54d94b3b5a7ea999798
Jessica <petroej1@ferris.edu>
2019-02-12 14:35:53 -0500

    Lets try this

--- FractelGen/Assets/BranchSegment.cs
+++ FractelGen/Assets/BranchSegment.cs
@@ -5,25 +5,48 @@ using UnityEditor;
 public class BranchSegment : MonoBehaviour {
 
     public Transform endPoint;
-    public BranchSegment prefabeBranch;
+    public Transform TopPosZ;
+    public Transform TopNegZ;
+    public Transform TopPosX;
+    public Transform TopNegX;
+    public Transform Mid1PosZ;
+    public Transform Mid1NegZ;
+    public Transform Mid1NegX;
+    public Transform Mid1PosX;
+    public Transform Mid2NegZ;
+    public Transform Mid2PosZ;
+    public Transform Mid2NegX;
+    public Transform Mid2PosX;
+    public Transform BotPosZ;
+    public Transform BotNegZ;
+    public Transform BotPosX;
+    public Transform BotNegX;
+    public BranchSegment prefabeCoral;
 	// Use this for initialization
 	void Start () {
 		
 	}
     public void Grow(float scale, Vector3 localEuler)
     {
-        if (scale < .3f) return;
-        BranchSegment newBranch = Instantiate(prefabeBranch, endPoint.position, Quaternion.identity, transform);
-        newBranch.transform.localScale = Vector3.one * scale;
-        newBranch.transform.localEulerAngles = localEuler;
-        newBranch.Grow(scale * .75f, new Vector3(0,0,-10));
-        newBranch.Grow(scale * .75f, new Vector3(0, 0, 10));
+        GameObject[] coral = GameObject.FindGameObjectsWithTag("polup");
+        Transform[] coralTransforms = new Transform[coral.Length];
+        for (int i = 0; i < coral.Length; i++)
+        {
+            coralTransforms[i] = coral[i].transform;
+            if (scale < .3f) return;
+            BranchSegment newCoral = Instantiate(prefabeCoral, coral[i].transform, Quaternion.identity, transform);
+            newCoral.transform.localScale = Vector3.one * scale;
+            newCoral.transform.localEulerAngles = localEuler;
+            newCoral.Grow(scale * .75f, new Vector3(0, 0, -10));
+        }
+       
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
 }
 [CustomEditor(typeof(BranchSegment))]
 public class BranchSegmentEditor : Editor
