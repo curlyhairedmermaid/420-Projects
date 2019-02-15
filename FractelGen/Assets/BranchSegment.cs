8a6cc2b1d6bfb9c235f2e5625afcf269ee4e645e
Jessica <petroej1@ferris.edu>
2019-02-14 20:58:51 -0500

    No more Memory Leak
    +It looks like coral
    +it stops
    +It rotates

--- FractelGen/Assets/BranchSegment.cs
+++ FractelGen/Assets/BranchSegment.cs
@@ -4,49 +4,69 @@ using UnityEngine;
 using UnityEditor;
 public class BranchSegment : MonoBehaviour {
 
-    public Transform endPoint;
-    public Transform TopPosZ;
-    public Transform TopNegZ;
-    public Transform TopPosX;
-    public Transform TopNegX;
-    public Transform Mid1PosZ;
-    public Transform Mid1NegZ;
-    public Transform Mid1NegX;
-    public Transform Mid1PosX;
-    public Transform Mid2NegZ;
-    public Transform Mid2PosZ;
-    public Transform Mid2NegX;
-    public Transform Mid2PosX;
-    public Transform BotPosZ;
-    public Transform BotNegZ;
-    public Transform BotPosX;
-    public Transform BotNegX;
     public BranchSegment prefabeCoral;
 	// Use this for initialization
-	void Start () {
-		
-	}
-    public void Grow(float scale, Vector3 localEuler)
+    public void Grow(float scale)
     {
-        GameObject[] coral = GameObject.FindGameObjectsWithTag("polup");
-        Transform[] coralTransforms = new Transform[coral.Length];
+        GrowPosZ(scale, new Vector3(0, 0, Random.Range(0, 45)));
+        GrowNegz(scale, new Vector3(0, 0, Random.Range(-45, 0)));
+        GrowPosX(scale, new Vector3(Random.Range(0, 45), 0, 0));
+        GrowNegX(scale, new Vector3(Random.Range(-45, 0), 0, 0));
+    }
+
+    public void GrowPosZ(float scale, Vector3 localEuler) {
+        GameObject[] coral = GameObject.FindGameObjectsWithTag("PosZPol");
+        float localScale = scale;
         for (int i = 0; i < coral.Length; i++)
         {
-            coralTransforms[i] = coral[i].transform;
-            if (scale < .3f) return;
-            BranchSegment newCoral = Instantiate(prefabeCoral, coral[i].transform, Quaternion.identity, transform);
+            if (localScale < .3f) return;
+            BranchSegment newCoral = Instantiate(prefabeCoral, coral[i].transform.position, Quaternion.identity, transform);
+            localScale *= .3f;
             newCoral.transform.localScale = Vector3.one * scale;
             newCoral.transform.localEulerAngles = localEuler;
-            newCoral.Grow(scale * .75f, new Vector3(0, 0, -10));
+            newCoral.GrowPosZ(localScale, new Vector3(0, 0, -10));
         }
-       
-        
     }
-	
-	// Update is called once per frame
-	void Update () {
+    public void GrowNegz(float scale, Vector3 localEuler) {
+        GameObject[] coral = GameObject.FindGameObjectsWithTag("NegZPol");
+        float localScale = scale;
+        for (int i = 0; i < coral.Length; i++)
+        {
+            if (localScale < .3f) return;
+            localScale *= .3f;
+            BranchSegment newCoral = Instantiate(prefabeCoral, coral[i].transform.position, Quaternion.identity, transform);
+            newCoral.transform.localScale = Vector3.one * scale;
+            newCoral.transform.localEulerAngles = localEuler;
+            newCoral.GrowNegz(localScale, new Vector3(0, 0, -10));
+        }
+    }
+    public void GrowPosX(float scale, Vector3 localEuler) {
+        GameObject[] coral = GameObject.FindGameObjectsWithTag("PosXPol");
+        float localScale = scale;
+        for (int i = 0; i < coral.Length; i++)
+        {
+            if (localScale < .3f) return;
+            localScale *= .3f;
+            BranchSegment newCoral = Instantiate(prefabeCoral, coral[i].transform.position, Quaternion.identity, transform);
+            newCoral.transform.localScale = Vector3.one * scale;
+            newCoral.transform.localEulerAngles = localEuler;
+            newCoral.GrowPosX(localScale, new Vector3(0, 0, -10));
+        }
+    }
+    public void GrowNegX(float scale, Vector3 localEuler) {
+        GameObject[] coral = GameObject.FindGameObjectsWithTag("NegXPol");
+        float localScale = scale;
+        for (int i = 0; i < coral.Length; i++)
+        {
+            if (localScale < .3f) return;
+            localScale *= .3f;
+            BranchSegment newCoral = Instantiate(prefabeCoral, coral[i].transform.position, Quaternion.identity, transform);
+            newCoral.transform.localScale = Vector3.one * scale;
+            newCoral.transform.localEulerAngles = localEuler;
+            newCoral.GrowNegX(localScale, new Vector3(0, 0, -10));
+        }
+    }
 
-    }
 }
 [CustomEditor(typeof(BranchSegment))]
 public class BranchSegmentEditor : Editor
@@ -58,7 +78,7 @@ public class BranchSegmentEditor : Editor
 
         if (GUILayout.Button("GROW"))
         {
-            (target as BranchSegment).Grow(1, Vector3.zero);
+            (target as BranchSegment).Grow(1);
 
         }
 
